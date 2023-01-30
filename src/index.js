de38fd94eb60f4c4a19086bf74fe012b2d8c43a1
hiwi-dci-student <hiwot.abdi@dci.education>
2023-01-30 15:43:39 +0100

    modal promise

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import "./css/style.css"; //importing css
+
+const box = document.querySelector(".modal");
+
+//writing promise
+
+const boxPromise = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("block");
+    }, 60000);
+  });
+};
+
+boxPromise().then((result) => {
+  box.style.display = result;
+});
